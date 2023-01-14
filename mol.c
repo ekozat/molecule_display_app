6528ef8c56d9e4e1f0bfa5ea5629ab7e2116220b
ekozatch <ekozatch@uoguelph.ca>
2023-01-14 18:35:23 -0500

    fixed atomset

--- mol.c
+++ mol.c
@@ -1,12 +1,18 @@
 #include "mol.h"
 
 void atomset (atom *atom, char element[3], double *x, double *y, double *z){
-    atom->x = &x;
-    atom->y = &y;
-    atom->z = &z;
+    atom->x = *x;
+    atom->y = *y;
+    atom->z = *z;
     strcpy(atom->element, element);
 }
 
+void atomget (atom *atom, char element[3], double *x, double *y, double *z){
+    x = &(atom->x)
+    y = &(atom->y)
+    z = &(atom->z)
+    strcpy(element, &(atom->))
+}
 
 
 
