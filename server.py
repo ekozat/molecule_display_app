f388cc80b22f74b946fe12de34cfecfbb6470d13
ekozatch <ekozatch@uoguelph.ca>
2023-02-26 23:40:39 -0500

    implemented do_get

--- server.py
+++ server.py
@@ -3,6 +3,20 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class MyHandler( BaseHTTPRequestHandler ):
     def do_GET(self):
+        if self.path == "/"
+            self.send_response(200) # OK response
+            self.send_header( "Content-type", "text/html" )
+            self.send_header( "Content-length", len(webform_page) )
+            self.end_headers()
+
+            self.wfile.write( bytes( webform_page, "utf-8" ))
+
+        else:
+            self.send_response( 404 )
+            self.end_headers()
+            self.wfile.write( bytes( "404: not found", "utf-8" ) )
+
+    #def DO_POST(self):
 
 
 
