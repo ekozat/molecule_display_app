3a5edbabe86c323fea3ecfa5b40b811c670064e6
ekozatch <ekozatch@uoguelph.ca>
2023-03-19 17:29:28 -0400

    removed dictionaries

--- MolDisplay.py
+++ MolDisplay.py
@@ -3,18 +3,6 @@ import io
 
 mol = molecule.molecule() #create new molecule object
 
-radius ={   'H': 25,
-            'C': 40,
-            'O': 40,
-            'N': 40,
-        }
-
-element_name = {'H': 'grey',
-                'C': 'black',
-                'O': 'red',
-                'N': 'blue',
-            }
-
 header = """<svg version="1.1" width="1000" height="1000"
 xmlns="http://www.w3.org/2000/svg">"""
 
