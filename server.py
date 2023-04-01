b061601615fbc13a5ebe52c11c08f65b679de0d8
ekozatch <ekozatch@uoguelph.ca>
2023-04-01 16:42:00 -0400

    initial launch

---
+++ server.py
@@ -0,0 +1,76 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+import io
+import sys
+import urllib
+
+import MolDisplay
+import molsql
+
+public_files = [ '/view.html', '/style.css', '/molecule.js' ];
+
+class MyHandler( BaseHTTPRequestHandler ):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200) # OK response
+            self.send_header( "Content-type", "text/html" )
+            self.send_header( "Content-length", len(webform_page) )
+            self.end_headers()
+
+            self.wfile.write( bytes( webform_page, "utf-8" ))
+        else:
+            self.send_response( 404 )
+            self.end_headers()
+            self.wfile.write( bytes( "404: not found", "utf-8" ) )
+
+    # implement
+    def do_POST(self):
+        if self.path == "/molecule":
+            # Parse the uploaded file into a Molecule object
+            content_len = int(self.headers.get('content-length', 0))
+
+            for i in range(4):
+              next(self.rfile)
+
+            mol = MolDisplay.Molecule()
+
+            # read it in as a textfile
+            textFile = io.TextIOWrapper(self.rfile, encoding='utf-8')
+            mol.parse(textFile)
+            
+            # Sort the atoms in the molecule
+            mol.sort()
+            
+            # Generate the SVG for the molecule
+            svg = mol.svg()
+            
+            # Send the SVG to the client
+            self.send_response(200)
+            self.send_header('Content-type', 'image/svg+xml')
+            self.send_header('Content-length', len(svg))
+            self.end_headers()
+            self.wfile.write(svg.encode())
+        else:
+            self.send_error(404, 'File Not Found')
+
+
+webform_page = """
+<html>
+  <head>
+    <title> Menu </title>
+  </head>
+  <body>
+    <h1> Molecule Displayer </h1>
+    <form action="molecule" enctype="multipart/form-data" method="post">
+      <p>
+        <input type="file" id="sdf_file" name="filename"/>
+      </p>
+      <p>
+        <input type="submit" value="Upload"/>
+      </p>
+    </form>
+  </body>
+</html>"""
+
+httpd = HTTPServer( ( 'localhost', int(sys.argv[1]) ), MyHandler )
+httpd.serve_forever()
