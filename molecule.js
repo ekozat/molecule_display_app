291274491c3c5e3f3c4aef98379ee7e5963458db
ekozatch <ekozatch@uoguelph.ca>
2023-04-07 13:58:18 -0400

    element handler added

--- molecule.js
+++ molecule.js
@@ -5,25 +5,33 @@ $(document).ready(
     /* this defines a function that gets called after the document is in memory */
   function()
   {
-
     /* add a click handler for our button */
-    $("#elementAdd").submit(
+    $("#elementAdd").click(
       function()
       {
-	/* ajax post (path, data, callback function) */ //new connection gets made from the browser to the server (post request)
-	$.post("/elements_handler.html",
-	  /* pass a JavaScript dictionary */ //key and value pairs = name: retrieves value (key)
-	  // keys are always strings in python = but in JS the key is not in quotations
-	  {
-	    num: $("#elementnum").val(),	/* retreive value of name field */
-	    extra_info: "some stuff here"
-	  },
-	  function( data, status )
-	  {
-	    alert( "Data: " + data + "\nStatus: " + status );
-		//$("$label").text(data); returns text content of element
-	  }
-	);
+		/* ajax post (path, data, callback function) */ //new connection gets made from the browser to the server (post request)
+		$.post("/elements_handler.html",
+		/* pass a JavaScript dictionary */ //key and value pairs = name: retrieves value (key)
+		// keys are always strings in python = but in JS the key is not in quotations
+		{
+			num: $("#elementnum").val(),	/* retreive value of name field */
+			code: $("#elementcode").val(),
+			name: $("#elementname").val(),
+			colour1: $("#colorpicker1").val(),
+			colour2: $("#colorpicker2").val(),
+			colour3: $("#colorpicker3").val(),
+			radius: $("#radius").val(),
+			extra_info: "some stuff here"
+		},
+		function( data, status )
+		{
+			alert( "Data: " + data + "\nStatus: " + status );
+			//$("$label").text(data); returns text content of element
+		}
+	).fail(function(err, status) {
+		alert("Something went wrong");
+		// something went wrong, check err and status
+	 });
       }
     );
   }
