c1d4188ccec1a3dc1ccbd3298d3f33752f5244e3
ekozatch <ekozatch@uoguelph.ca>
2023-04-09 01:06:43 -0400

    made naming clear

--- molecule.js
+++ molecule.js
@@ -6,7 +6,7 @@ $(document).ready(
   function()
   {
     /* !!!HANDER for element addition and deletion */
-    $("#elementbutton").click( 
+    $("#element_button").click( 
       function()
       {
 		/* ajax post (path, data, callback function) */ //new connection gets made from the browser to the server (post request)
@@ -14,9 +14,9 @@ $(document).ready(
 		/* pass a JavaScript dictionary */ //key and value pairs = name: retrieves value (key)
 		// keys are always strings in python = but in JS the key is not in quotations
 		{
-			enum: $("#elementnum").val(),	/* retreive value of name field */
-			ecode: $("#elementcode").val(),
-			ename: $("#elementname").val(),
+			enum: $("#element_num").val(),	/* retreive value of name field */
+			ecode: $("#element_code").val(),
+			ename: $("#element_name").val(),
 			ecolour1: $("#colorpicker1").val(),
 			ecolour2: $("#colorpicker2").val(),
 			ecolour3: $("#colorpicker3").val(),
@@ -36,14 +36,14 @@ $(document).ready(
     );
     
     /* HANDLER for all sdf document uploads*/
-    $("#uploadbutton").click( 
+    $("#upload_button").click( 
       function()
       {
     $.post("/sdf_handler.html",
     /* pass a JavaScript dictionary */ //key and value pairs = name: retrieves value (key)
     {
       fp: $("#sdf").val(),
-      mname: $("#molname").val()
+      mname: $("#mol_name").val()
     },
     function( data, status )
     {
